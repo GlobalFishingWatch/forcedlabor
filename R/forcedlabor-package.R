7277259b32260580f4a48904b33116d5911d3f96
rociojoo <rocio.joo@globalfishingwatch.org>
2021-11-19 15:03:01 -0700

    add=FALSE in globalVariables

--- R/forcedlabor-package.R
+++ R/forcedlabor-package.R
@@ -5,4 +5,4 @@
 ## usethis namespace: end
 NULL
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
-if(getRversion() >= "2.15.1")  utils::globalVariables(c(".", "where"))
+if(getRversion() >= "2.15.1")  utils::globalVariables(c(".", "where"), add = FALSE)
