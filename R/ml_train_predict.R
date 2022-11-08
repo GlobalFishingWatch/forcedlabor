70921b0f34caaf6f94b7e5654a2c81e4ae583bf7
rociojoo <rocio.joo@globalfishingwatch.org>
2022-11-08 10:22:40 -0700

    unnesting predictions in ml_train_predict

--- R/ml_train_predict.R
+++ R/ml_train_predict.R
@@ -717,8 +717,11 @@ ml_train_predict <- function(training_df, fl_rec, rf_spec, cv_splits_all,
                                              tidyr::unnest(.data$predictions)
 
                                            return(cv_predictions)
-                                         }, .options = furrr::furrr_options(seed = TRUE)))
+                                         }, .options = furrr::furrr_options(seed = TRUE))) |>
 
+      # Remove unnecessary columns
+      dplyr::select(-.data$recipe_seed, -.data$fl_recipe) |>
+      tidyr::unnest(.data$predictions)
 
   }
 
