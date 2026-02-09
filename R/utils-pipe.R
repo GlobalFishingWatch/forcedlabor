835ceab5b5b2bea816ce06b1f976d3a1a8666415
Guille <guillermo.gonzalez@globalfishingwatch.org>
2026-02-09 09:26:53 +0000

    Updating global variables. Worth discussing

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -2,3 +2,6 @@
 globalVariables(c("known_offender"))
 globalVariables(c("pred_class"))
 globalVariables(c(".estimate"))
+globalVariables(c("source_id_number"))
+globalVariables(c("common_seed"))
+globalVariables(c("indID"))
