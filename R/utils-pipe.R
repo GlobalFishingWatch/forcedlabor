c234a04195425b1e9ec4e5009b3e38b5680982bc
Guille <guillermo.gonzalez@globalfishingwatch.org>
2026-02-09 09:35:49 +0000

    Identifying the origin of global variables, and adding id

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -1,7 +1,8 @@
 
-globalVariables(c("known_offender"))
+globalVariables(c("known_offender")) # from data("fl_training")
 globalVariables(c("pred_class"))
 globalVariables(c(".estimate"))
-globalVariables(c("source_id_number"))
-globalVariables(c("common_seed"))
-globalVariables(c("indID"))
+globalVariables(c("source_id_number")) # from data("fl_training")
+globalVariables(c("indID")) # from data("fl_training")
+globalVariables(c("common_seed")) # named in common_seed_tibble
+globalVariables(c("id")) # from rsample::group_vfold_cv
