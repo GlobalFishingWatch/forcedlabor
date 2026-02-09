fe685edde06e1bcbbcf3dd8a0925d90167a2eccc
Guille <guillermo.gonzalez@globalfishingwatch.org>
2026-02-09 09:39:38 +0000

    Update working script

--- data-raw/Guille_test.R
+++ data-raw/Guille_test.R
@@ -1,4 +1,4 @@
-# rm(list = ls())
+rm(list = ls())
 #
 #
 # # from the using-package Rmd ----------------------------------------------
@@ -21,39 +21,31 @@ rows_pred <- sample(1:dim(fl_training)[1], size = 1000)
 fl_predict <- fl_training[rows_pred,]
 fl_training <- fl_training[-rows_pred,]
 
-#GM: this seems redundant as the known_non_offender is already a factor str(fl_training)
-fl_training$known_non_offender <- as.factor(fl_training$known_non_offender)
-
-#GM: Why this line of code?
-levels(fl_training$known_non_offender) <-
-  c(levels(fl_training$known_non_offender),1)
-
-
 #GM: Why this is not wrapped in a function?
-num_folds <- 5
-num_bags <- 2 #5
-down_sample_ratio <- 1
-num_common_seeds <- 2# 3
-common_seed_tibble <- tibble::tibble(common_seed =
-                                       seq(1:num_common_seeds) * 101)
+# num_folds <- 5
+#num_bags <- 2 #5
+#down_sample_ratio <- 1
+#num_common_seeds <- 2# 3
+#common_seed_tibble <- tibble::tibble(common_seed =
+#                                       seq(1:num_common_seeds) * 101)
 
 
 # Run all common_seeds
-bag_runs <- common_seed_tibble |>
-  tidyr::crossing(tibble::tibble(bag = seq(num_bags))) |>
-  dplyr::mutate(recipe_seed = dplyr::row_number() * common_seed) |>
-  dplyr::mutate(counter = dplyr::row_number())
+#bag_runs <- common_seed_tibble |>
+#  tidyr::crossing(tibble::tibble(bag = seq(num_bags))) |>
+#  dplyr::mutate(recipe_seed = dplyr::row_number() * common_seed) |>
+#  dplyr::mutate(counter = dplyr::row_number())
 
 ## Cross Validation
 # Ensure there is no splitting across source_id across analysis and assessment
 # data sets.  Need to make separate splits for each seed.
-cv_splits_all <- common_seed_tibble |>
-  dplyr::mutate(cv_splits = purrr::map(common_seed, function(x) {
-    set.seed(x)
-    rsample::group_vfold_cv(fl_training,
-                            group = source_id_number,
-                            v = num_folds)
-  }))
+#cv_splits_all <- common_seed_tibble |>
+#  dplyr::mutate(cv_splits = purrr::map(common_seed, function(x) {
+#    set.seed(x)
+#    rsample::group_vfold_cv(fl_training,
+#                            group = source_id_number,
+#                            v = num_folds)
+# }))
 #GM: data in each split: cv_splits_all$cv_splits[[1]]$splits[[1]]$data
 
 #test<-apply(common_seed_tibble, MARGIN = 1, FUN = function(x){
@@ -99,17 +91,23 @@ oopts <- options(future.globals.maxSize = 10200*1024^2)  ## 15 GB
 # bag_downsample: get a recipe with downsampling for each bag and corresponding seed (nested within the cv_setup)
 # cv_setup: set ups the workflow and cv folds. Returns cv_workflow, cv_folds, seed and bag (for training the model in ml_train)
 # ml_train: train and predict over the test set. Return each fitted model and the predicted df of scores over the test set
-# source("./R/dev_cv_setup.R")
-# source("./R/dev_bag_downsample.R")
-# source("./R/dev_ml_train.R")
+#source("./R/dev_cv_setup.R")
+#source("./R/dev_bag_downsample.R")
+#source("./R/dev_ml_train.R")
+#source("./R/dev_ml_load.R")
+#source("./R/dev_ml_predict.R")
 
 # GM: testing the functions over the first 5 bags (3 seeds)
 tictoc::tic()
-cv_df<- dev_cv_setup(bag_runs = bag_runs,
-                 cv_splits_all = cv_splits_all,
-                 fl_rec = fl_rec,
-                 rf_spec = rf_spec,
-                 down_sample_ratio = down_sample_ratio)
+cv_df<- dev_cv_setup(training_data = fl_training,
+                       num_folds = 5,
+                       num_bags = 2,
+                       num_seeds = 2,
+                       fl_rec = fl_rec,
+                       rf_spec = rf_spec,
+                       down_sample_ratio = 1,
+                       tune_parameters = FALSE,
+                       num_grid = 5)
 train_test <- dev_ml_train(cv_setup = cv_df,
                            free_cores = free_cores,
                            parallel_plan = parallel_plan,
@@ -117,11 +115,24 @@ train_test <- dev_ml_train(cv_setup = cv_df,
 tictoc::toc() #6.773 sec elapsed # 250 sec for 5 bags
 
 
+loaded_models <- dev_ml_load(cv_setup = cv_df,
+                             save_dir = "./models/test")
+
+
+predictions1 = dev_ml_predict(trained_models = loaded_models,
+               new_data = fl_predict,
+               free_cores = free_cores,
+               parallel_plan = parallel_plan)
+
+#predictions2 = dev_ml_predict(trained_models = train_test$fitted_models,
+#                              new_data = fl_predict,
+#                              free_cores = free_cores,
+#                              parallel_plan = parallel_plan)
 
 
 # GM: original ml_train_predict function
 # Running it for the first two bags only
-# source("./R/ml_train_predict.R")
+source("./R/ml_train_predict.R")
 tictoc::tic()
 train_pred_proba2 <- ml_train_predict(
   fl_rec = fl_rec,
@@ -132,25 +143,16 @@ train_pred_proba2 <- ml_train_predict(
   parallel_plan = parallel_plan,
   free_cores = free_cores,
   prediction_df = fl_predict,
-  save_dir = "models"
+  save_dir = "./models",
 )
 tictoc::toc()
 
+all(train_test$train_probabilities$.pred_1 == train_pred_proba[train_pred_proba$holdout == 0,]$.pred_1)
+all(predictions1$.pred_1 == train_pred_proba[train_pred_proba$holdout == 1,]$.pred_1)
 
 #GM:
 #Checking .pred1 (predictions over the test set are equivalent in both methods)
 all(train_pred_proba2$.pred_1 == train_test$train_pred_proba$.pred_1)
-
-# GM:
-# Function to read saved models based on cv_folds and save_dir:
-ml_read_fit <- function(cv_folds,
-                        save_dir){
-  lapply(cv_df, function(x){
-    sapply(x$cv_folds$id, function(y){
-      readRDS(file=file.path(save_dir,paste0("rf_seed", x$seed, "_bag", x$bag, "_", y, ".rds")))
-    },simplify = F)
-  })
-}
 
 ml_fitted_new <- ml_read_fit(cv_folds=cv_df$cv_folds,save_dir = "./models/test")
 ml_fitted_original <- ml_read_fit(cv_folds=cv_df$cv_folds,save_dir = "./models")
