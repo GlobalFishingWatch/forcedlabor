3bdcb73358c30fab7f04e2447ea5b1040b1e6d0f
Guille <guillermo.gonzalez@globalfishingwatch.org>
2026-02-09 08:49:19 +0000

    Including new arguments in dev_cv_setup from raw bags, folds, seeds, rather than outputs of the pipes outside of functions. Rename old function until fully shure is functional

--- R/old_dev_cv_setup.R
+++ R/old_dev_cv_setup.R
@@ -1,11 +1,7 @@
-#' Setting up the data structure to train the RF model based on a number of folds, bags and common seeds.
-#' If tune_parameters is provided  return..., otherwise return analysis/assessment splits and model workflows
-#' across CV folds and bags.
+#' Defining analysis/assessment splits and model workflows across CV folds and bags.
 #'
-#' @param training_data Dataset over which to generate CV folds and tuning grid
-#' @param num_folds Number of cross validation folds
-#' @param num_bags Number of bags
-#' @param num_seeds Number of common seeds
+#' @param bag_runs Tibble defining bag numbers and seeds
+#' @param cv_splits_all Cross-validation data splits
 #' @param fl_rec Model recipe
 #' @param rf_spec Random forest classifier specifications
 #' @param down_sample_ratio See under_ratio in ?themis::step_downsample. To reduce the weight of the unlabeled cases in the model, we randomly
@@ -16,54 +12,27 @@
 #'
 #' @returns List object containing cv_folds (analysis/assessment), model workflow and seed/bag identifiers
 #'
-#' @importFrom dplyr filter mutate row_number select
+#' @importFrom dplyr filter mutate select
 #' @importFrom purrr map pluck
-#' @importFrom rsample group_vfold_cv
 #' @importFrom themis step_downsample
-#' @importFrom tibble tibble
-#' @importFrom tidyr crossing
 #' @importFrom tune control_resamples tune_grid
 #' @importFrom workflows add_model add_recipe workflow
 #' @importFrom yardstick metric_set roc_auc
 #'
 #' @export
 
-dev_cv_setup2 <- function(training_data,
-                          num_folds,
-                          num_bags,
-                          num_seeds,
-                          fl_rec,
-                          rf_spec,
-                          down_sample_ratio,
-                          tune_parameters = FALSE,
-                          num_grid = 5)
+dev_cv_setup <- function(bag_runs,
+                     cv_splits_all,
+                     fl_rec,
+                     rf_spec,
+                     down_sample_ratio,
+                     tune_parameters = FALSE,
+                     num_grid = 5)
 {
 
-  common_seed_tibble <- tibble::tibble(common_seed =
-                                         seq(1:num_seeds) * 101)
-
-  # Run all common_seeds
-  # GM: probably merge dev_bag_downsample with this pipe
-  bag_runs <- common_seed_tibble |>
-    tidyr::crossing(tibble::tibble(bag = seq(num_bags))) |>
-    dplyr::mutate(recipe_seed = dplyr::row_number() * common_seed) |>
-    dplyr::mutate(counter = dplyr::row_number())
-
   down_bags<-dev_bag_downsample(bag_runs = bag_runs,
-                                fl_rec = fl_rec,
-                                down_sample_ratio = down_sample_ratio)
-
-  ## Cross Validation
-  # Ensure there is no splitting across source_id across analysis and assessment
-  # data sets.  Need to make separate splits for each seed.
-  # GM: training_data MUST have a source_id_number
-  cv_splits_all <- common_seed_tibble |>
-    dplyr::mutate(cv_splits = purrr::map(common_seed, function(x) {
-      set.seed(x)
-      rsample::group_vfold_cv(training_data,
-                              group = source_id_number,
-                              v = num_folds)
-    }))
+                            fl_rec = fl_rec,
+                            down_sample_ratio = down_sample_ratio)
 
   out<-purrr::pmap(list(down_bags$fl_recipe,
                         down_bags$common_seed,
@@ -74,14 +43,7 @@ dev_cv_setup2 <- function(training_data,
                      # Ensure all bags look the same
                      set.seed(y)
 
-                     if(tune_parameters && is.null(num_grid)){
-
-                       stop("num_grid must be provided when tune_parameters = TRUE")
-
-                     } else if (tune_parameters && !is.null(num_grid)) {
-                       # GM: I am not sure what the tune::tune_grid is doing
-
-                       message("Performing parameter tuning with ", num_grid, " grid points")
+                     if(tune_parameters){
 
                        cv_splits <- cv_splits_all |>
                          dplyr::filter(.data$common_seed == y) |>
@@ -111,8 +73,7 @@ dev_cv_setup2 <- function(training_data,
                          cv_predictions
                        )
 
-                       } else if (!tune_parameters) {
-
+                     } else {
                        # specifying the workflow with the model, recipe for data and how the
                        # tuning goes
                        cv_predictions_workflow <-
