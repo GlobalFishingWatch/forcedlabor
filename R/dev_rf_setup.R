19a91e770ffe7ea4a607fb1272c481002433efab
Andrea Sánchez Tapia <4183062+AndreaSanchezTapia@users.noreply.github.com.>
2026-03-12 10:51:35 -0700

    adding dev_rf :)

---
+++ R/dev_rf_setup.R
@@ -0,0 +1,53 @@
+#' Sets up the random forests that will be enter the DEDPUL classification
+#'
+#' @param training_data A tibble with training data
+#' @param y Character, name of the response variable. Defaults to `"known_offender"`
+#' @param x Vector with names of the features to be used. Defaults to NULL and should be set accordingly to avoid adding new features without knowing
+#' @param id
+#' @param dont_use
+#' @param control
+#' @param corr_threshold
+#' @param rf_trees
+#' @param rf_mtry
+#' @param rf_min_n
+#' @param rf_reg_factor
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+dev_rf_setup <- function(training_data = fl_training,
+                         y = "known_offender", #response
+                         x = NULL,
+                         id = "indID",
+                         dont_use = c("flag_region", "known_non_offender"),
+                         control = "source_id_number",
+                         corr_threshold = 0.75,
+                         rf_trees = 500,
+                         rf_mtry = 1,
+                         rf_min_n = 15,
+                         rf_reg_factor = 0.5) {
+  rf_recipe <- recipes::recipe(training_data) |>
+    recipes::update_role(y,
+                         new_role = "outcome") |>
+    recipes::update_role(tidyselect::all_of(x),
+                         new_role = "predictor") |>
+    recipes::update_role(tidyselect::all_of(id),,
+                         new_role = "id") |>
+    recipes::update_role(tidyselect::all_of(dont_use),
+                         new_role = "dont_use")  |>
+    recipes::update_role(tidyselect::all_of(control), new_role = "control")  |>
+    recipes::step_nzv(recipes::all_predictors())  |>
+    recipes::step_corr(recipes::all_numeric(), threshold = corr_threshold)
+
+  rf_spec <-
+    parsnip::rand_forest(trees = rf_trees,
+                         mtry = rf_mtry,
+                         min_n = rf_min_n) |>
+    parsnip::set_mode("classification") |>
+    parsnip::set_engine("ranger",
+                        regularization.factor = rf_reg_factor)
+  return(list(rf_recipe = rf_recipe,
+              rf_spec = rf_spec))
+
+}
